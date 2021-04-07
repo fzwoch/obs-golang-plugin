934d60cac66c24d07adafa39601deeb27b866a7e
Florian Zwoch <fzwoch@gmail.com>
2021-04-07 19:07:04 +0200

    its c symbols.. use underscores

--- plugin.go
+++ plugin.go
@@ -5,7 +5,7 @@ package main
 // #include <obs/obs-module.h>
 //
 // typedef char* (*get_name_t)(void* type_data);
-// extern char* getName(void* type_data);
+// extern char* get_name(void* type_data);
 //
 // typedef void* (*create_t)(obs_data_t* settings, obs_source_t* source);
 // extern void* create(obs_data_t* settings, obs_source_t* source);
@@ -14,19 +14,19 @@ package main
 // extern void destroy(void* data);
 //
 // typedef obs_properties_t* (*get_properties_t)(void* data);
-// extern obs_properties_t* getProperties(void* data);
+// extern obs_properties_t* get_properties(void* data);
 //
 // typedef void (*get_defaults_t)(obs_data_t* settings);
-// extern void getDefaults(obs_data_t* settings);
+// extern void get_defaults(obs_data_t* settings);
 //
 // typedef void (*video_render_t)(void* data, gs_effect_t* effect);
-// extern void videoRender(void* data, gs_effect_t* effect);
+// extern void video_render(void* data, gs_effect_t* effect);
 //
 // typedef uint32_t (*get_width_t)(void* data);
-// extern uint32_t getWidth(void* data);
+// extern uint32_t get_width(void* data);
 //
 // typedef uint32_t (*get_height_t)(void* data);
-// extern uint32_t getHeight(void* data);
+// extern uint32_t get_height(void* data);
 //
 // typedef void (*update_t)(void* data, obs_data_t* settings);
 // extern void update(void* data, obs_data_t* settings);
@@ -73,22 +73,22 @@ var source = C.struct_obs_source_info{
 	_type:        C.OBS_SOURCE_TYPE_INPUT,
 	output_flags: C.OBS_SOURCE_ASYNC_VIDEO | C.OBS_SOURCE_AUDIO | C.OBS_SOURCE_DO_NOT_DUPLICATE,
 
-	get_name: C.get_name_t(unsafe.Pointer(C.getName)),
+	get_name: C.get_name_t(unsafe.Pointer(C.get_name)),
 	create:   C.create_t(unsafe.Pointer(C.create)),
 	destroy:  C.destroy_t(unsafe.Pointer(C.destroy)),
 
-	get_properties: C.get_properties_t(unsafe.Pointer(C.getProperties)),
-	get_defaults:   C.get_defaults_t(unsafe.Pointer(C.getDefaults)),
-	video_render:   C.video_render_t(unsafe.Pointer(C.videoRender)),
-	get_width:      C.get_width_t(unsafe.Pointer(C.getWidth)),
-	get_height:     C.get_height_t(unsafe.Pointer(C.getHeight)),
+	get_properties: C.get_properties_t(unsafe.Pointer(C.get_properties)),
+	get_defaults:   C.get_defaults_t(unsafe.Pointer(C.get_defaults)),
+	video_render:   C.video_render_t(unsafe.Pointer(C.video_render)),
+	get_width:      C.get_width_t(unsafe.Pointer(C.get_width)),
+	get_height:     C.get_height_t(unsafe.Pointer(C.get_height)),
 	update:         C.update_t(unsafe.Pointer(C.update)),
 	show:           C.show_t(unsafe.Pointer(C.show)),
 	hide:           C.hide_t(unsafe.Pointer(C.hide)),
 }
 
-//export getName
-func getName(typeData unsafe.Pointer) *C.char {
+//export get_name
+func get_name(typeData unsafe.Pointer) *C.char {
 	return C.CString("OBS Golang Plugin")
 }
 
@@ -115,29 +115,29 @@ func destroy(data unsafe.Pointer) {
 	C.free(data)
 }
 
-//export getProperties
-func getProperties(data unsafe.Pointer) *C.obs_properties_t {
+//export get_properties
+func get_properties(data unsafe.Pointer) *C.obs_properties_t {
 	properties := C.obs_properties_create()
 	return properties
 }
 
-//export getDefaults
-func getDefaults(settings *C.obs_data_t) {
+//export get_defaults
+func get_defaults(settings *C.obs_data_t) {
 
 }
 
-//export videoRender
-func videoRender(data unsafe.Pointer, effect *C.gs_effect_t) {
+//export video_render
+func video_render(data unsafe.Pointer, effect *C.gs_effect_t) {
 
 }
 
-//export getWidth
-func getWidth(data unsafe.Pointer) C.uint32_t {
+//export get_width
+func get_width(data unsafe.Pointer) C.uint32_t {
 	return 0
 }
 
-//export getHeight
-func getHeight(data unsafe.Pointer) C.uint32_t {
+//export get_height
+func get_height(data unsafe.Pointer) C.uint32_t {
 	return 0
 }
 
